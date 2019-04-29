f0576ffaf3e02e201db56ae534d77f8f3b732f80
Rafael Rivadeneira <rafariva@gmail.com>
2019-04-29 09:54:08 -0500

    4 digit id, an remove bad

--- view_captures.py
+++ view_captures.py
@@ -5,16 +5,16 @@ from datetime import datetime
 sub_folders = ["cam1","cam2","cam3"]
 dataset_folder = "dataset"
 
-min = 999
+min = 9999
 max = 0
 
 for i in range(len(sub_folders)):
 	if os.path.exists(dataset_folder+"/"+sub_folders[i]):
 		files = [img for img in os.listdir(dataset_folder+"/"+sub_folders[i]) if img.endswith(".jpg")]
-		if int(files[0][:3])<min: 
-			min = int(files[0][:3])
-		if int(files[-1][:3])>max: 
-			max = int(files[-1][:3])
+		if int(files[0][:4])<min: 
+			min = int(files[0][:4])
+		if int(files[-1][:4])>max: 
+			max = int(files[-1][:4])
   else:
     print("folders no detected")
     exit()
@@ -24,7 +24,7 @@ rango = range(min,max+1)
 ind = rango[0]
 
 while ind in rango:
-	index = str(ind).zfill(3)
+	index = str(ind).zfill(4)
 	for i in range(len(sub_folders)):
 		file = dataset_folder+"/"+sub_folders[i]+"/"+index+".jpg"
 		if os.path.isfile(file):
@@ -38,7 +38,10 @@ while ind in rango:
 	elif key == 103: #(g)ood
 		pass
 	elif key == 98: #(b)ad
-		pass
+		for i in range(len(sub_folders)):
+			file = dataset_folder+"/"+sub_folders[i]+"/"+index+".jpg"
+			if os.remove(file):
+				ind+=1
 	elif key == 32 or key == 8: #(b)ack
 		ind -= 1
 	else: #any or enter
